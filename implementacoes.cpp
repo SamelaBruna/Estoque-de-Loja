b6e70e8d17775a3603633f0f3f59c58829c2f511
Sam <samelabrunaferreira@hotmail.com>
2018-09-10 12:13:37 -0300

    ListaDVD implementada

--- implementacoes.cpp
+++ implementacoes.cpp
@@ -249,7 +249,7 @@ void ListaCD::criar(unsigned Num)
   x = ( N>0 ? new CD [N] : NULL );
 }
 
-void ListaCD::copiar(const ListaLivro &LL)
+void ListaCD::copiar(const ListaCD &LCD)
 {
   criar(LCD.N);
   for (unsigned i=0; i<N; i++) x[i] = LCD.x[i];
@@ -287,7 +287,7 @@ void ListaCD::incluir(const CD &C)
      cout << "CD incluido com sucesso!\n";
 }
 
-void ListaLivro::excluir(unsigned id){
+void ListaCD::excluir(unsigned id){
      if (id>=N )
      {
           cerr << "Indice invalido\n";
@@ -351,5 +351,115 @@ void ListaCD::salvar(ostream &O) const
      return O;
 
 }
+
+void ListaDVD::criar(unsigned Num)
+{
+  N = Num;
+  x = ( N>0 ? new DVD [N] : NULL );
+}
+
+void ListaCD::copiar(const ListaCD &LDV)
+{
+  criar(LDV.N);
+  for (unsigned i=0; i<N; i++) x[i] = LDV.x[i];
+}
+
+void ListaDVD::limpar()
+{
+     if(x==NULL)
+     {
+          N = 0;
+     }
+     else
+     {
+          delete[] x;
+          x = NULL;
+          N = 0;
+
+     }
+ListaDVD::ListaDVD(unsigned Num)
+{
+  criar(Num);
+  for (unsigned i=0; i<N; i++) x[i] = "";
+}
+
+void ListaDVD::incluir(const DVD &DV)
+{
+
+     DVD *provisorio;
+     provisorio = new DVD [N+1];
+     for(unsigned i=0; i<N; i++) provisorio[i] = x[i];
+     provisorio[N] = DV;
+     N = N+1;
+     delete [] x;
+     x = provisorio;
+     cout << "DVD incluido com sucesso!\n";
+}
+
+void ListaDVD::excluir(unsigned id){
+     if (id>=N )
+     {
+          cerr << "Indice invalido\n";
+
+     }
+     if (N == 1)
+     {
+          delete [] x;
+          N = 0;
+          x = NULL;
+          cout << "DVD excluido com sucesso!\n";
+          return;
+     }
+     DVD *provisorio;
+     provisorio = new DVD [N-1];
+     for(unsigned i=0; i<N-1; i++)
+     {
+          if(i >= id)provisorio[i] = x[i+1];
+
+          else provisorio[i]= x[i];
+
+    }
+    N = N-1;
+    delete [] x;
+    x = prov;
+    cout << "DVD excluido com sucesso!\n";
+
+}
+ostream ListaDVD::imprimir(ostream &O) const
+{
+
+     cout << "LISTADVD" << N << endl;
+
+     for (unsigned i=0; i<this.N; i++)
+     {
+          O << i << ")";
+          x[i].imprimir(O);
+          O << endl;
+     }
+
+     return O;
+}
+void ListaDVD::ler(istream &I)
+{
+    I >> N;
+    I.ignore(numeric_limits<streamsize>::max(), '\n');
+    x = new DVD [N];
+    for(unsigned i=0; i<N; i++) x[i].ler(I);
+
+}
+void ListaDVD::salvar(ostream &O) const
+{
+ cout << "LISTADVD" << N << endl;
+
+     for (unsigned i=0; i<this.N; i++)
+     {
+          x[i].imprimir(O);
+          O << endl;
+     }
+
+     return O;
+
+}
+
 
 
